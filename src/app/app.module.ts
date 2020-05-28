e04275cd256717c7064fac665f35cdec1b8daa5b
FemFX <ignedko34@gmail.com>
2020-05-28 13:20:53 +0300

    app

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
+import { AppComponent } from "./app.component";
+import { TodosComponent } from "./todos/todos.component";
+import { TodoFormComponent } from "./todo-form/todo-form.component";
+import { TodosFilterPipe } from './shared/todos-filter.pipe.ts';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodosComponent,
+    TodoFormComponent,
+    TodosFilterPipe,
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
